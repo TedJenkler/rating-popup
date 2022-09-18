00ee363579af21d09716dc3fe818e2dc42669f84
Teodor Jenkler <108183860+TedJenkler@users.noreply.github.com>
2022-09-18 18:13:10 +0200

    Update thx.js

--- thx.js
+++ thx.js
@@ -1,1 +1,6 @@
-// How to import in vanila js?
+let vote = localStorage.getItem("hasVoted")
+let rating = document.querySelector('#rating')
+
+rating.innerHTML = `You selected ${vote} out of 5`
+
+console.log(vote)
